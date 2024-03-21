6a14ad2ded39c8ae6d5a91efa764a7eee3f0bbc4
Alisa Sofianova <alr2097@gmail.com>
2024-03-21 11:35:26 +0200

    Initialization Laravel

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    // return view('welcome');
+    return view('index');
+});
+
+Route::get('/hello', function () {
+    return 'Hello';
+});
+
+Route:: get('/greet/{name}', function ($name) {
+    return 'Hello '.$name;
+});
+
+Route::fallback(function () {
+    return "still got somewhere";
+});
