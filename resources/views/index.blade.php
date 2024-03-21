be71cddc67c4b817eac6ce7546198b33e35ea0c0
Alisa Sofianova <alr2097@gmail.com>
2024-03-21 12:14:32 +0200

    task blade directives

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,23 @@
-Hello! I am a blade template!
+<div>Hello! I am a blade template!</div>
 
-@isset($name)
-    <div>The name is: {{$name}}<div>
-@endisset
+<div>
+    {{-- @if(count($tasks))
+        @foreach ($tasks as $task)
+            <div>{{$task->title}}</div>
+        @endforeach
+    @else
+        <div>There are none task</div>
+    @endif --}}
+
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id'=>$task->id])}}">{{$task->title}}</a>
+        </div>
+
+    @empty
+        <div>There is no tasks</div>
+    @endforelse
+
+</div>
+
 
